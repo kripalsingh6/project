bc46cc45f7cce89bcc723c0b2eeed0056ae7847f
kripal <thakurkripalsingh6@gmail.com>
2025-11-09 15:23:05 +0530

    Expressfile

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const port = 8080;
 
 
 
@@ -9,6 +9,44 @@ app.listen(port, () => {
 });
 
 
-app.use(()=>{
-    console.log("request recived");
+// app.use((req,res)=>{
+//     console.log("request recived");
+//     let code= "<h1>Fruits</h1> <ul><li>Apple</li> <li>Orange</li></ul>";
+//     res.send(code);
+// });
+
+app.get("/",(req,res)=>{
+  res.send("helo now i change my road");
+});
+app.get("/apple",(req,res)=>{
+  res.send("you contacted apple path");
+});
+app.get("/orange",(req,res)=>{
+  res.send("you contacted orange path");
+});
+// app.get("*",(req,res)=>{
+//   res.send("this path is not exist");
+// });
+
+app.post("/post",(req,res)=>{
+    res.send("you send me the response");
+});
+
+app.get("/:Username/:Id",(req,res)=>{
+   let {Username,Id}=req.params;
+  // console.log(req.params);
+  // res.send("welcome to jira world");
+  let htmlstr=`<h1>welcome to jira world @ ${Username}</h1>`;
+  res.send(htmlstr);
+
+  
+});
+
+app.get("/Search",(req,res)=>{
+  let{q}= req.query;
+  let val= `<h1>welocome to the searching ${q}</h1>`;
+  if(!q){
+    res.send("<h1>Nothing searched</h1>");
+  }
+  res.send(val);
 });
